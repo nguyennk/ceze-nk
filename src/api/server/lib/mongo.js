557687103feb9ab6de1a736af1bb0926d3fd4fbb
Nguyen Ken <1538969+nguyennk@users.noreply.github.com>
2018-07-17 09:15:26 +0700

    initial commit

---
+++ src/api/server/lib/mongo.js
@@ -0,0 +1,40 @@
+const settings = require('./settings');
+const winston = require('winston');
+const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
+
+const mongodbConnection = settings.mongodbServerUrl;
+const mongoPathName = url.parse(mongodbConnection).pathname;
+const dbName = mongoPathName.substring(mongoPathName.lastIndexOf('/') + 1);
+
+const RECONNECT_INTERVAL = 1000;
+const CONNECT_OPTIONS = {
+  reconnectTries: 3600,
+  reconnectInterval: RECONNECT_INTERVAL,
+  useNewUrlParser: true
+}
+
+const onClose = () => {
+  winston.info('MongoDB connection was closed');
+}
+
+const onReconnect = () => {
+  winston.info('MongoDB reconnected');
+}
+
+const connectWithRetry = () => {
+  MongoClient.connect(mongodbConnection, CONNECT_OPTIONS, (err, client) => {
+    if(err){
+      winston.error('MongoDB connection was failed:', err.message);
+      setTimeout(connectWithRetry, RECONNECT_INTERVAL);
+    } else {
+      const db = client.db(dbName);
+      db.on('close', onClose);
+      db.on('reconnect', onReconnect);
+      module.exports.db = db;
+      winston.info('MongoDB connected successfully');
+    }
+  });
+};
+
+connectWithRetry();
